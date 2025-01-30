dfc91d09fc15d3209af8f8573d5be74640d1b4d0
Guilherme Gomes <ggomes.jua.ti@gmail.com>
2025-01-30 19:40:56 -0300

    :tada: init: first commit with the project structure

---
+++ api-desafio/src/main/java/com/ggomes12/api_desafio/ApiDesafioApplication.java
@@ -0,0 +1,13 @@
+package com.ggomes12.api_desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDesafioApplication.class, args);
+	}
+
+}
---
+++ api-desafio/src/test/java/com/ggomes12/api_desafio/ApiDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggomes12.api_desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
