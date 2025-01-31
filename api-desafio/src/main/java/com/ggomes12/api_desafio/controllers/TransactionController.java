b09316e8ce33e06ed5c317ad225193ae49ab04f3
Guilherme Gomes <ggomes.jua.ti@gmail.com>
2025-01-30 21:08:48 -0300

    :sparkles: feat: Adding controller of the Transaction

---
+++ api-desafio/src/main/java/com/ggomes12/api_desafio/controllers/TransactionController.java
@@ -0,0 +1,40 @@
+package com.ggomes12.api_desafio.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ggomes12.api_desafio.business.services.TransactionService;
+import com.ggomes12.api_desafio.controllers.dtos.TransactionRequestDTO;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/transacao")
+@RequiredArgsConstructor
+public class TransactionController {
+	
+	private final TransactionService transactionService;
+	
+	@PostMapping
+	public ResponseEntity<Void> addTransaction(@RequestBody TransactionRequestDTO dto) {
+
+		transactionService.addTransaction(dto);
+
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+	
+	
+	@DeleteMapping
+	public ResponseEntity<Void> clearTransactions() {
+
+		transactionService.clearTransactions();
+
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}
+
+}
