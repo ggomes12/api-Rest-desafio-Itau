f034229288ee861e2e1c5f8c3e9967e954defc27
Guilherme Gomes <ggomes.jua.ti@gmail.com>
2025-01-30 21:48:03 -0300

    :books: docs: Adding swagger to document the API

--- api-desafio/src/main/java/com/ggomes12/api_desafio/controllers/StatisticController.java
+++ api-desafio/src/main/java/com/ggomes12/api_desafio/controllers/StatisticController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ggomes12.api_desafio.business.services.StatisticService;
 import com.ggomes12.api_desafio.controllers.dtos.StatisticResponseDTO;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -19,6 +22,12 @@ public class StatisticController {
 	private final StatisticService statisticService;
 
 	 @GetMapping
+	 @Operation(description = "Get statistics of transactions")
+		@ApiResponses(value = {
+				@ApiResponse(responseCode = "200", description = "Statistics retrieved"),
+				@ApiResponse(responseCode = "400", description = "Invalid search interval"),
+				@ApiResponse(responseCode = "500", description = "Internal server error")
+				})
 	 public ResponseEntity<StatisticResponseDTO> getStatistics(@RequestParam(value = "searchInterval", 
 	 												required = false, defaultValue = "60") 
 	 													Integer searchInterval) {
--- api-desafio/src/main/java/com/ggomes12/api_desafio/controllers/TransactionController.java
+++ api-desafio/src/main/java/com/ggomes12/api_desafio/controllers/TransactionController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ggomes12.api_desafio.business.services.TransactionService;
 import com.ggomes12.api_desafio.controllers.dtos.TransactionRequestDTO;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -21,6 +24,14 @@ public class TransactionController {
 	private final TransactionService transactionService;
 	
 	@PostMapping
+	@Operation(description = "Add a new transaction")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "201", description = "Transaction added"),
+			@ApiResponse(responseCode = "204", description = "Transaction older than 60 seconds"),
+			@ApiResponse(responseCode = "400", description = "Invalid transaction data"),
+			@ApiResponse(responseCode = "422", description = "Transaction timestamp is in the future"),
+			@ApiResponse(responseCode = "500", description = "Internal server error")	
+	})
 	public ResponseEntity<Void> addTransaction(@RequestBody TransactionRequestDTO dto) {
 		
 		transactionService.addTransaction(dto);
@@ -30,6 +41,12 @@ public class TransactionController {
 	
 	
 	@DeleteMapping
+	@Operation(description = "Clear all transactions")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Transactions cleared"),
+			@ApiResponse(responseCode = "400", description = "Invalid transaction data"),
+			@ApiResponse(responseCode = "500", description = "Internal server error")	
+			})
 	public ResponseEntity<Void> clearTransactions() {
 
 		transactionService.clearTransactions();
