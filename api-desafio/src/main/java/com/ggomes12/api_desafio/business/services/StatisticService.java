90bc760b82600671dd5f957a84e201cc8ffd52c4
Guilherme Gomes <ggomes.jua.ti@gmail.com>
2025-01-30 21:00:28 -0300

    :sparkles: feat: Adding the service and the DTO of the Statistic

---
+++ api-desafio/src/main/java/com/ggomes12/api_desafio/business/services/StatisticService.java
@@ -0,0 +1,35 @@
+package com.ggomes12.api_desafio.business.services;
+
+import java.util.DoubleSummaryStatistics;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.ggomes12.api_desafio.controllers.dtos.StatisticResponseDTO;
+import com.ggomes12.api_desafio.controllers.dtos.TransactionRequestDTO;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class StatisticService {
+	
+	public final TransactionService transactionService;
+	
+	public StatisticResponseDTO calculateStatistics(Integer searchInterval) {
+        
+        List<TransactionRequestDTO> transactions = transactionService.searchTransactions(searchInterval);
+        
+        DoubleSummaryStatistics statisticTransactions = transactions.stream()
+    		   .mapToDouble(TransactionRequestDTO::valor).summaryStatistics();
+        
+		return new StatisticResponseDTO(statisticTransactions.getCount(), 
+				statisticTransactions.getSum(),
+				statisticTransactions.getAverage(), 
+				statisticTransactions.getMin(), 
+				statisticTransactions.getMax());
+        
+	}
+	
+
+}
---
+++ api-desafio/src/main/java/com/ggomes12/api_desafio/controllers/dtos/StatisticResponseDTO.java
@@ -0,0 +1,7 @@
+package com.ggomes12.api_desafio.controllers.dtos;
+
+
+
+public record StatisticResponseDTO(Long count, Double sum, Double avg, Double min, Double max) {
+
+}
